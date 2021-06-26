5a4aa0ae3f2ecf2d682461221831f3d55dc0e1dd
@penta2019 <>
2021-06-26 09:56:19 +0900

    mahjongsoul url

--- script/mahjong_soul.js
+++ script/mahjong_soul.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://game.mahjongsoul.com/
+// @match        https://game.mahjongsoul.com/*
 // @grant        none
 // ==/UserScript==
 

52b4318fd49db00f2bc8afaf5fdb3be2049d7bdb
penta2019 <>
2022-09-12 09:32:25 +0900

    listener.Debugの実装

--- src/listener/debug.rs
+++ src/listener/debug.rs
@@ -11,7 +11,36 @@ impl Debug {
 }
 
 impl Listener for Debug {
-    fn notify_event(&mut self, _stg: &Stage, _event: &Event) {
-        prompt();
+    fn notify_event(&mut self, stg: &Stage, event: &Event) {
+        loop {
+            print!("debug");
+            let input = prompt();
+            let args: Vec<&str> = input.split_whitespace().collect();
+            // println!("{:?}", args);
+            if args.len() == 0 {
+                break;
+            }
+
+            match args[0] {
+                "p" => {
+                    println!("{}", stg);
+                }
+                "e" => {
+                    println!("{:?}", event);
+                }
+                "s" => {
+                    println!("{:?}", stg);
+                }
+                _ => {
+                    println!("unknown command: {}\n", args[0]);
+                    println!("usage");
+                    println!("[Enter]: next step");
+                    println!("p: print stage info");
+                    println!("e: debug-print current Event");
+                    println!("s: debug-print Stage")
+                }
+            }
+            println!();
+        }
     }
 }
